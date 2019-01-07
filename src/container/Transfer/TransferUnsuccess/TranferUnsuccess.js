bac8cf2bf0532d0cbd068e72372566975ae57f63
oktavi <oktavisilitonga14@gmail.com>
2019-01-07 17:46:39 +1100

    content transfer

--- src/container/Transfer/TransferUnsuccess/TranferUnsuccess.js
+++ src/container/Transfer/TransferUnsuccess/TranferUnsuccess.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react';
 import TitlePages from '../../../components/UI/TitlePages/TitlePages';
 import Auz from '../../../hoc/Auz/Auz';
 import BodyClass from 'react-body-classname';
+import ContentTransferUnsuccess from '../../../components/Content/Transfer/TransferUnsuccess/TransferUnsuccess';
 
 class TranferUnsuccess extends Component {
   render() {
     return (
       <div>
-        <TitlePages title={"welcome"} />
-        <BodyClass className="no-transition welcome-page" >
+        <TitlePages title={"Transfer Gagal | Transfer to Mobile - Permata Bank"} />
+        <BodyClass className="no-transition transfer-page" >
           <Auz>
-          <h1>Content TranferUnsuccess</h1>
+            <ContentTransferUnsuccess />
           </Auz>
         </BodyClass>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import '../src/assets/css/app.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
