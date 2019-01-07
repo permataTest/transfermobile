1ba83c7dc1f3e46eb457cc5627d0fcba3b784967
wahyudisetiaji <wahyudisetiaji@gmail.com>
2019-01-07 17:05:09 +0700

    update title head

--- src/components/UI/Background/Background.js
+++ src/components/UI/Background/Background.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ImagesBackground from '../../../assets/images/background/body-desktop.png';
 
 const Background = (props) =>  (
-    <img src={ImagesBackground} className="bg-theme"  alt="background" />
+    <img src={ImagesBackground} className={[props.classBG]}  alt="background" />
 )
 
 export default Background;
