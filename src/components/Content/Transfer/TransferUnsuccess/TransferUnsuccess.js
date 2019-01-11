eb6adc6560fe3985a0013f846d4ccf162c841ed4
HelmiYogantara <helmiyogantara@gmail.com>
2019-01-11 11:06:15 +0700

    res confilct

--- src/components/Content/Transfer/TransferUnsuccess/TransferUnsuccess.js
+++ src/components/Content/Transfer/TransferUnsuccess/TransferUnsuccess.js
@@ -4,7 +4,6 @@ import LogoUnsuccess from "../../../../assets/images/icon/transfer-unsuccessful.
 import { Link } from "react-router-dom";
 
 class TransferUnsuccess extends Component {
-<<<<<<< HEAD
   render() {
     return (
       <div>
@@ -26,36 +25,6 @@ class TransferUnsuccess extends Component {
       </div>
     );
   }
-=======
-
-    render() {
-        return (
-            <div>
-                <Background classBG={"bg-theme"} />
-                <div className="site-content">
-                    <Logo />
-                    <Card>
-                        <div className="container-content">
-                            <div className="transfer-icon">
-                                <img src={LogoUnsuccess} alt="Transfer Success" />
-                            </div>
-                            <div className="bank-account-name">
-                                <p>
-                                    <strong>{this.props.receiver}</strong><br />
-                                    Kamu tidak berhasil melakukan transfer
-					            </p>
-                            </div>
-                            <div className="wrapper-button">
-                                <Link to="/detailbank" className="button button-primary">Ulangi Proses</Link>
-                            </div>
-                        </div>
-                        <SideBackground />
-                    </Card>
-                </div>
-            </div>
-        )
-    }
->>>>>>> ref button detail
 }
 
 const mapStateToProps = state => {
