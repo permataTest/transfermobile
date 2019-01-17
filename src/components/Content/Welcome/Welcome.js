f5c9b5e2c17b3cb16c1211efba757c1e5149fb5f
HelmiYogantara <helmiyogantara@gmail.com>
2019-01-17 10:51:06 +0700

    fix error

--- src/components/Content/Welcome/Welcome.js
+++ src/components/Content/Welcome/Welcome.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Welcome extends Component {
 
   render() {
     return (
       <div>
-        {enterRedirect}
         <p>
           Halo, Selamat datang di{" "}
           <strong className="color-primary">PermataBank</strong>
