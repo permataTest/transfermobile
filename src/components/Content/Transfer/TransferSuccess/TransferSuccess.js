ac100834fed8b62f328232a47933945e8ea059ab
iivwijaya <iiv.wijaya@gmail.com>
2019-01-17 16:30:15 +0700

    wwwwwwwwwww

--- src/components/Content/Transfer/TransferSuccess/TransferSuccess.js
+++ src/components/Content/Transfer/TransferSuccess/TransferSuccess.js
@@ -3,8 +3,27 @@ import { connect } from "react-redux";
 
 import LogoSuccess from "../../../../assets/images/icon/transfer-success.svg";
 
+// 3790046900
+
 class TransferSuccess extends Component {
+
   render() {
+    let acountNo    = localStorage.getItem('rekening')
+    let bankName    = localStorage.getItem('namaB')
+    let accountName = localStorage.getItem('accountN')
+    if (this.props.AccountNo){
+      localStorage.setItem('rekening', this.props.AccountNo)
+      localStorage.setItem('namaB', this.props.BankName)
+      localStorage.setItem('accountN', this.props.AccountName)
+    }
+    // if (sessionStorage.clickcount) {
+    //   sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1;
+    // } else {
+    //   sessionStorage.clickcount = 1;
+    // }
+    // document.getElementById("result").innerHTML = "You have clicked the button " +
+    // sessionStorage.clickcount + " time(s) in this session.";
+
     return (
       <div>
         <div className="transfer-icon">
@@ -12,16 +31,22 @@ class TransferSuccess extends Component {
         </div>
         <div className="bank-account-name">
           <p>
-            <strong>{this.props.AccountName},</strong>
+          {
+            this.props.AccountName? <strong>{this.props.AccountName},</strong>: <strong>{accountName},</strong>
+          }
             <br />
             Transaksi telah berhasil dilakukan ke
           </p>
         </div>
         <div className="bank-detail">
           <p>
-            {this.props.BankName}
+            {
+              this.props.BankName? <span> {this.props.BankName} </span>: <span>{bankName}</span>
+            }
             <br />
-            <strong>{this.props.AccountNo}</strong>
+            {
+              this.props.AcountNo? <strong> {this.props.AccountNo} </strong>: <strong>{acountNo}</strong>
+            }
           </p>
         </div>
       </div>
